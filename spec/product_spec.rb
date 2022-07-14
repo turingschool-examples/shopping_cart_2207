8f79c5aff4bba8b5f27b0fd78503817712ea0dfa
Rebecka Hendricks <rebecka.hendricks@gmail.com>
2022-07-14 16:01:23 -0600

    Add attributes category, name, unit_price, quantity to product class

--- spec/product_spec.rb
+++ spec/product_spec.rb
@@ -3,31 +3,31 @@ require './lib/product'
 
 describe Product do
   context 'Iteration 1' do
-    xit 'Product exists' do
+    it 'Product exists' do
       product = Product.new(:paper, 'toilet paper', 3.70, '10')
 
       expect(product).to be_an_instance_of Product
     end
 
-    xit 'Product has a category' do
+    it 'Product has a category' do
       product = Product.new(:paper, 'toilet paper', 3.70, '10')
 
       expect(product.category).to eq :paper
     end
 
-    xit 'Product has a name' do
+    it 'Product has a name' do
       product = Product.new(:paper, 'toilet paper', 3.70, '10')
 
       expect(product.name).to eq 'toilet paper'
     end
 
-    xit 'Product has a unit price' do
+    it 'Product has a unit price' do
       product = Product.new(:paper, 'toilet paper', 3.70, '10')
 
       expect(product.unit_price).to eq 3.70
     end
 
-    xit 'Product has a quantity' do
+    it 'Product has a quantity' do
       product = Product.new(:paper, 'toilet paper', 3.70, '10')
 
       expect(product.quantity).to eq 10
