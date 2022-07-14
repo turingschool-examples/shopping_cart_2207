50cecb41266ec9d1df1aea4cbaf1a9a8134780a1
Thomas Musselman <tmusselman@gmail.com>
2022-07-13 20:57:31 -0600

    feat: implement product class

---
+++ lib/product.rb
@@ -0,0 +1,17 @@
+class Product
+    attr_reader :category,
+                :name,
+                :unit_price,
+                :quantity
+
+    def initialize(category, name, unit_price, quantity)
+        @category = category
+        @name = name
+        @unit_price = unit_price
+        @quantity = quantity.to_i
+    end
+
+    def total_price
+        @quantity * @unit_price
+    end
+end
