5ada391dbde7c4d07c9bbbac2b26db016bb9bdf7
Kevin Ta <kvnta@yahoo.com>
2022-07-13 22:01:38 -0600

    Complete iteration 3, time ran out (over time by about 5 min estimate?)

--- lib/shopping_cart.rb
+++ lib/shopping_cart.rb
@@ -2,9 +2,44 @@ class ShoppingCart
     attr_reader :name,
                 :capacity,
                 :products
-    def initialize(name, capacity, products)
+                
+    def initialize(name, capacity)
         @name = name 
-        @capacity = capacity
+        @capacity = capacity.delete("items")
         @products = []
+        @is_full = false
+    end
+
+    def add_product(product)
+        @products << product
+    end
+
+    def details
+        cart_details ={}
+        cart_details[:name] = name
+        cart_details[:capacity] = capacity.to_i
+        cart_details
+    end
+
+    def is_full?
+        if total_number_of_products >= 30
+            true
+        else
+            false
+        end
+    end
+
+    def total_number_of_products
+        total_quantity = 0
+        @products.each do |product|
+            product.quantity 
+            total_quantity += product.quantity
+        end
+         total_quantity
+       
+    end
+
+    def products_by_category(category)
+        @products.find_all {|product| product.category == category}
     end
 end
--- spec/shopping_cart_spec.rb
+++ spec/shopping_cart_spec.rb
@@ -7,6 +7,8 @@ RSpec.describe ShoppingCart do
       @cart = ShoppingCart.new("King Soopers", "30items") 
        @product1 = Product.new(:paper, 'toilet paper', 3.70, '10')
        @product2 = Product.new(:meat, 'chicken', 4.50, '2') 
+       @product3 = Product.new(:paper, 'tissue paper', 1.25, '1')
+       @product4 = Product.new(:produce, 'apples', 0.99, '20')
     end
     it 'exists' do 
         expect(@cart).to be_a(ShoppingCart)
@@ -27,7 +29,35 @@ RSpec.describe ShoppingCart do
 
     it 'can show the cart details in hash ..' do 
         @cart.add_product(@product1)
-        @cart.add_product(@product2)
         expect(@cart.details).to eq({name: "King Soopers", capacity: 30})
     end
+
+    # ITERATION Three(3)
+
+    it 'display total number of products in cart & see if cart is full' do
+        @cart.add_product(@product1)
+        @cart.add_product(@product2)
+        @cart.add_product(@product3)
+
+        expect(@cart.total_number_of_products).to eq(13)
+        expect(@cart.is_full?).to eq(false)
+    end
+
+    it 'can now see if cart is full?' do 
+         @cart.add_product(@product1)
+        @cart.add_product(@product2)
+        @cart.add_product(@product3)
+        @cart.add_product(@product4)
+        expect(@cart.is_full?).to eq(true)
+    end
+
+    it 'can show products by category' do 
+        @cart.add_product(@product1)
+        @cart.add_product(@product2)
+        @cart.add_product(@product3)
+        @cart.add_product(@product4)
+        expect(@cart.products_by_category(:paper)).to eq([@product1, @product3])
+    end
+
+
 end
