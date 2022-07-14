129bc865eaae2f018903cd43a9e9147975891ef8
A.J. Krumholz <krumholz.alan@gmail.com>
2022-07-14 08:48:25 -0400

    complete Iteration 4 and tests

--- lib/shopping_cart.rb
+++ lib/shopping_cart.rb
@@ -33,4 +33,22 @@ class ShoppingCart
       product.category == category
     end
   end
+
+  def percentage_occupied
+    decimal = (total_number_of_products.to_f / @capacity)
+    output = sprintf('%.2f', (decimal * 100))
+    output.to_f
+  end
+
+  def sorted_products_by_quantity
+    products.sort_by do |product|
+      product.quantity
+    end.reverse
+  end
+
+  def product_breakdown
+    products.group_by do |product|
+      product.category
+    end
+  end
 end
--- spec/shopping_cart_spec.rb
+++ spec/shopping_cart_spec.rb
@@ -62,4 +62,29 @@ RSpec.describe ShoppingCart do
     expect(cart.products_by_category(:paper)).to eq([product1, product3])
   end
 
+  it 'can display percent occupied' do
+    cart.add_product(product1)
+    cart.add_product(product2)
+    cart.add_product(product3)
+
+    expect(cart.percentage_occupied).to eq(43.33)
+  end
+
+  it 'can sort products by quantity' do
+    cart.add_product(product1)
+    cart.add_product(product2)
+    cart.add_product(product3)
+    cart.add_product(product4)
+
+    expect(cart.sorted_products_by_quantity).to eq([product4, product1, product2, product3])
+  end
+
+  it 'can provide a product breakdown' do
+    cart.add_product(product1)
+    cart.add_product(product2)
+    cart.add_product(product3)
+    cart.add_product(product4)
+
+    expect(cart.product_breakdown).to eq({meat: [product2], paper: [product1, product3], produce: [product4]})
+  end
 end
