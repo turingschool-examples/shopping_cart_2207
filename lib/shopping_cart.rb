a9f1264a7746ba42161b4e5eb7f8e345866f3590
Alex M <Alexm717@hotmail.com>
2022-07-15 00:30:10 -0700

    final commit iter4. almost complete

--- lib/shopping_cart.rb
+++ lib/shopping_cart.rb
@@ -18,4 +18,24 @@ class ShoppingCart
   def total_number_of_products
     products.map { |product| product.quantity }.sum
   end
+
+  def is_full?
+    if total_number_of_products >= @capacity
+      true
+    else
+      false
+    end
+  end
+
+  def products_by_category(category)
+    products.select { |product| product.category == category }
+  end
+
+  def percent_occupied
+    ((total_number_of_products / @capacity.to_f).truncate(4)) * 100
+  end
+
+  def sorted_products_by_quantity
+    products.sort_by { |product| product.quantity }.reverse
+  end
 end
