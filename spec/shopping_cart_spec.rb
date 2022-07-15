15b71474de1db42ee1ff6a01e44f382a14075ffa
Alex M <Alexm717@hotmail.com>
2022-07-14 23:12:59 -0700

    test for end of iter.2 and start iter.3

--- spec/shopping_cart_spec.rb
+++ spec/shopping_cart_spec.rb
@@ -6,6 +6,9 @@ describe(Product) do
   context("Iteration 2") do
     before(:each) do
       @cart = ShoppingCart.new("King Soopers", "30items")
+      @product1 = Product.new(:paper, "toilet paper", 3.70, "10")
+      @product2 = Product.new(:meat, "chicken", 4.50, "2")
+      @product3 = Product.new(:paper, "tissue paper", 1.25, "1")
     end
 
     it("exisits") do
@@ -22,6 +25,25 @@ describe(Product) do
 
     it("has products") do
       expect(@cart.products).to(eq([]))
+    end
+
+    it("can add products") do
+      @cart.add_product(@product1)
+      @cart.add_product(@product2)
+      expect(@cart.products).to(eq([@product1, @product2]))
+    end
+
+    it("has details") do
+      @cart.add_product(@product1)
+      @cart.add_product(@product2)
+      expect(@cart.details).to(eq({name: "King Soopers", capacity: 30}))
+    end
+
+    it("can give a total count of products") do
+      @cart.add_product(@product1)
+      @cart.add_product(@product2)
+      @cart.add_product(@product3)
+      expect(@cart.total_number_of_products).to(eq(13))
     end
   end
 end
