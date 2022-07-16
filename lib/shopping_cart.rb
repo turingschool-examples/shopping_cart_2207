79a7ff55a45eae75895eee6ce3e4bb3795dbea2c
Alaina Kneiling <alaina.noel.8@gmail.com>
2022-07-16 12:43:03 -0600

    Iteration 2 complete

--- lib/shopping_cart.rb
+++ lib/shopping_cart.rb
@@ -1,10 +1,17 @@
 class ShoppingCart
-  attr_reader :name, :capacity, :products
+  attr_reader :name, :capacity, :products, :details
 
   def initialize(name, capacity)
     @name = name
     @capacity = capacity
     @products = []
+    @details = Hash.new
+    @details[:name] = name
+    @details[:capacity] = capacity.delete_suffix("items").to_i
+  end
+
+  def add_product(product)
+    @products << product
   end
 
 end
--- spec/shopping_cart_spec.rb
+++ spec/shopping_cart_spec.rb
@@ -28,6 +28,22 @@ describe ShoppingCart do
       expect(cart.products).to eq []
     end
 
+    it 'has products as an empty array to start' do
+      cart = ShoppingCart.new("King Soopers", "30items")
+      product1 = Product.new(:paper, 'toilet paper', 3.70, '10')
+      product2 = Product.new(:meat, 'chicken', 4.50, '2')
+      cart.add_product(product1)
+      cart.add_product(product2)
+
+      expect(cart.products).to eq [product1, product2]
+    end
+
+    it 'has details' do
+      cart = ShoppingCart.new("King Soopers", "30items")
+
+      expect(cart.details).to eq({name: "King Soopers", capacity: 30})
+    end
+
 
   end
 end
