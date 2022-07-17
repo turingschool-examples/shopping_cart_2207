4e785ba75be5505fb919206a88c8fef8cccac5bc
Astrid Hecht <silentaviary@gmail.com>
2022-07-16 22:20:28 -0600

    feat: Iteration 1 complete

--- lib/product.rb
+++ lib/product.rb
@@ -1,1 +1,22 @@
- 
+class Product
+  attr_reader :category, :name
+
+  def initialize(category, name, price, quantity)
+    @category = category
+    @name = name
+    @price = price.to_s
+    @quantity = quantity
+  end
+
+  def unit_price
+    @price.to_f
+  end
+
+  def quantity 
+    @quantity.to_i 
+  end
+
+  def total_price
+    @price.to_f * @quantity.to_f
+  end
+end
