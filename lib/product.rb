5d0dc7a1cf6ae3f8a63f1fccee21cc335e062518
Jake Kim <imjakekim@gmail.com>
2022-07-16 22:56:10 -0700

    finish iteration 1

--- lib/product.rb
+++ lib/product.rb
@@ -7,4 +7,10 @@ class Product
     @unit_price = unit_price
     @quantity = quantity.to_i
   end
+
+  def total_price
+    total = 0.0
+    quantity.times {total += @unit_price}
+    return total
+  end
 end
