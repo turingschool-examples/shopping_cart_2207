4bc99e3a6e11fcc645c98f4a91e06a411e244214
Jake Kim <imjakekim@gmail.com>
2022-07-16 23:03:16 -0700

    jk now im done with iteration 1. didnt see the hoard method in the interaction pattern

--- lib/product.rb
+++ lib/product.rb
@@ -6,11 +6,20 @@ class Product
     @name = name
     @unit_price = unit_price
     @quantity = quantity.to_i
+    @hoarded = false
   end
 
   def total_price
     total = 0.0
     quantity.times {total += @unit_price}
     return total
+  end
+
+  def is_hoarded?
+    @hoarded
+  end
+
+  def hoard
+    @hoarded = true
   end
 end
