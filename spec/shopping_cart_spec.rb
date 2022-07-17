a9ac4913d533d91ac921774915c6c1fb0ab162fb
Gavin Carew <gjcarew@gmail.com>
2022-07-17 09:22:39 -0700

    Add to shopping_cart_spec to satisfy iteration 3 requirements

--- spec/shopping_cart_spec.rb
+++ spec/shopping_cart_spec.rb
@@ -7,6 +7,8 @@ RSpec.describe do
     @cart = ShoppingCart.new("King Soopers", "30items")
     @product1 = Product.new(:paper, 'toilet paper', 3.70, '10')
     @product2 = Product.new(:meat, 'chicken', 4.50, '2')
+    @product3 = Product.new(:paper, 'tissue paper', 1.25, '1')
+    @product4 = Product.new(:produce, 'apples', 0.99, '20')
   end
 
   it 'exists' do
@@ -28,12 +30,38 @@ RSpec.describe do
   it 'can add products to the cart' do
     @cart.add_product(@product1)
     @cart.add_product(@product2)
+
     expect(@cart.products).to eq([@product1, @product2])
   end
 
   it 'has a method to see cart name and capacity as a hash' do
     expect(@cart.details).to eq({name: "King Soopers", capacity: 30})
   end
+
+  it 'can tell total number of products' do
+    @cart.add_product(@product1)
+    @cart.add_product(@product2)
+    @cart.add_product(@product3)
+
+    expect(@cart.total_number_of_products).to eq(13)
+  end
+
+  xit 'has a method to check if the cart is full' do
+    @cart.add_product(@product1)
+    @cart.add_product(@product2)
+    @cart.add_product(@product3)
+
+    expect(@cart.is_full?).to be false
+
+    @cart.add_product(@product4)
+
+    expect(@cart.is_full?).to be true
+  end
+
+  xit 'can filter products by category' do
+    expect(@cart.products_by_category(:paper)).to eq([@product1, @product3])
+  end
+
 
 
 
