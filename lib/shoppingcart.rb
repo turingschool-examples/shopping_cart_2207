5b3838aeea7b0b6ca20d3fc4fb0e116258930156
dominicod <dominicodonnell99@gmail.com>
2022-07-17 15:40:15 -0500

    Iteration 4 complete

--- lib/shoppingcart.rb
+++ lib/shoppingcart.rb
@@ -15,4 +15,23 @@ class ShoppingCart
     def details
         details = {:name => @name, :capacity => @capacity}
     end
+
+    def total_number_of_products
+        total = @products.map {|product| product.quantity}.sum
+    end
+
+    def is_full?
+        return true if total_number_of_products >= @capacity
+        false
+    end
+
+    def products_by_category(category)
+        categorized = []
+        @products.each do |product|
+            if product.category == category
+                categorized << product
+            end
+        end
+        categorized
+    end
 end
