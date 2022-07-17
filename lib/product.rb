981977590b1b56a7a7482963430dc864df9cceb7
Kaelin Sleevi <kaelin.sleevi@outlook.com>
2022-07-17 15:45:43 -0600

    add @hoarded and @hoard tests and methods

--- lib/product.rb
+++ lib/product.rb
@@ -2,17 +2,26 @@ class Product
   attr_reader :category,
               :name,
               :quantity,
-              :unit_price
+              :unit_price,
+              :hoarded
 
   def initialize(category, name, unit_price, quantity)
     @category = category
     @name = name
     @unit_price = unit_price
     @quantity = quantity.to_i
+    @hoarded = hoarded
   end
 
   def total_price
     unit_price * quantity
   end
 
+  def is_hoarded?
+    @hoarded == false
+  end
+
+  def hoard
+    @hoarded == true
+  end
 end
