b2ccc6f01c3645976ccb742e0042531735328839
Sandy Griffin <Sandra.Griffin@colorado.edu>
2022-07-16 20:40:12 -0600

    test: added all tests needed for iteration patterns in iteration 3

--- spec/shopping_cart_spec.rb
+++ spec/shopping_cart_spec.rb
@@ -35,4 +35,34 @@ RSpec.describe ShoppingCart do
             expect(@cart.details).to eq({name: "King Soopers", capacity: 30})
         end
     end
+
+    context 'Iteration 3' do
+        before :each do
+            @cart = ShoppingCart.new("King Soopers", "30items")
+            @product1 = Product.new(:paper, 'toilet paper', 3.70, '10')
+            @product2 = Product.new(:meat, 'chicken', 4.50, '2') 
+            @product3 = Product.new(:paper, 'tissue paper', 1.25, '1')
+            @product4 = Product.new(:produce, 'apples', 0.99, '20')
+        end
+
+        it 'can return the total number of products added' do
+            expect(@cart.total_number_of_products).to eq(13)
+        end
+
+        it 'can return when the cart is NOT full' do
+            @cart.add_product(@product1)
+            @cart.add_product(@product2)
+            @cart.add_product(@product3)
+            expect(@cart.is_full?).to eq(false)
+        end
+
+        it 'can return that the cart IS full' do
+            @cart.add_product(@product4)
+            expect(@cart.is_full?).to eq(true)
+        end
+
+        it 'will return the products of a given category' do
+            expect(@cart.products_by_category(:paper).count).to eq(2)
+        end
+    end
 end
