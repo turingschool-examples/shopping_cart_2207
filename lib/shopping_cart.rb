036216300ee9dba928ac216ed28a3909f010aff3
Astrid Hecht <silentaviary@gmail.com>
2022-07-16 23:07:54 -0600

    feat: Iteration 3 complete

--- lib/shopping_cart.rb
+++ lib/shopping_cart.rb
@@ -17,4 +17,20 @@ class ShoppingCart
   def add_product(product)
     @products << product
   end
+
+  def total_number_of_products
+    total = 0
+    @products.each do |product|
+      total += product.quantity
+    end
+    total
+  end
+
+  def is_full?
+    total_number_of_products >= capacity
+  end
+
+  def products_by_category(category)
+    @products.find_all { |product| product.category == category }
+  end
 end
