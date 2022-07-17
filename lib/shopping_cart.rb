129c12c49797aeccb631d212df105fb1782f6fcd
Jake Kim <imjakekim@gmail.com>
2022-07-16 23:37:09 -0700

    finish shopping cart checks if it exceeds its capacity

--- lib/shopping_cart.rb
+++ lib/shopping_cart.rb
@@ -1,14 +1,22 @@
 require './lib/product'
 class ShoppingCart
-  attr_reader :name, :capacity, :products, :details
+  attr_reader :name, :capacity, :products,
+              :details, :total_number_of_products
   def initialize(name, capacity)
     @name = name
     @capacity = capacity.chomp('items').to_i
     @products = []
     @details = {name: name, capacity: @capacity}
+    @total_number_of_products = 0
   end
 
   def add_product(product)
     @products << product
+    @total_number_of_products += product.quantity
   end
+
+  def is_full?
+    @total_number_of_products > @capacity
+  end
+  
 end
