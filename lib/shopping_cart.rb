e4b0d691aec707d0ca7b3d8b3a951c09f9eb6fb6
Jake Kim <imjakekim@gmail.com>
2022-07-16 23:27:20 -0700

    finish iteration 2

--- lib/shopping_cart.rb
+++ lib/shopping_cart.rb
@@ -1,7 +1,14 @@
+require './lib/product'
 class ShoppingCart
-  attr_reader :name, :capacity
+  attr_reader :name, :capacity, :products, :details
   def initialize(name, capacity)
     @name = name
     @capacity = capacity.chomp('items').to_i
     @products = []
+    @details = {name: name, capacity: @capacity}
+  end
+
+  def add_product(product)
+    @products << product
+  end
 end
--- spec/shopping_cart_spec.rb
+++ spec/shopping_cart_spec.rb
@@ -1,30 +1,47 @@
-require 'spec'
+require 'rspec'
 require './lib/shopping_cart'
 
-describe ShoppingCart
+describe ShoppingCart do
   context 'Iteration 2' do
     it 'ShoppingCart exists' do
-      shopping_cart = ShoppingCart.new('King Soopers', '30items')
+      cart = ShoppingCart.new('King Soopers', '30items')
 
-      expect(shopping_cart).to be_an_instance_of(ShoppingCart)
+      expect(cart).to be_an_instance_of(ShoppingCart)
     end
 
     it 'ShoppingCart has a name' do
-      shopping_cart = ShoppingCart.new('King Soopers', '30items')
+      cart = ShoppingCart.new('King Soopers', '30items')
 
-      expect(shopping_cart.name).to eq('King Soopers')
+      expect(cart.name).to eq('King Soopers')
     end
 
     it 'ShoppingCart has a capacity' do
-      shopping_cart = ShoppingCart.new('King Soopers', '30items')
+      cart = ShoppingCart.new('King Soopers', '30items')
 
-      expect(shopping_cart.capacity).to eq(30)
+      expect(cart.capacity).to eq(30)
     end
 
     it 'ShoppingCart has products' do
-      shopping_cart = ShoppingCart.new('King Soopers', '30items')
+      cart = ShoppingCart.new('King Soopers', '30items')
 
-      expect(shopping_cart.products).to eq([])
+      expect(cart.products).to eq([])
     end
 
+    it 'ShoppingCart can add products' do
+      product1 = Product.new(:paper, 'toilet paper', 3.70, '10')
+      product2 = Product.new(:meat, 'chicken', 4.50, '2')
+      cart = ShoppingCart.new('King Soopers', '30items')
+
+      cart.add_product(product1)
+      cart.add_product(product2)
+
+      expect(cart.products).to eq([product1, product2])
+    end
+
+    it 'ShoppingCart can access its details' do
+      cart = ShoppingCart.new('King Soopers', '30items')
+
+      expect(cart.details).to eq({name: "King Soopers", capacity: 30})
+    end
   end
+end
