d5acaed502e69a9fc896a63922c0f05216f8ae6d
Madeline Mauser <madeline.mauser@gmail.com>
2022-07-18 10:03:09 -0600

    Add tests for ShoppingCart class

--- spec/shopping_cart_spec.rb
+++ spec/shopping_cart_spec.rb
@@ -1,1 +1,48 @@
+require 'rspec'
+require './lib/shopping_cart'
+require './lib/product'
 
+describe ShoppingCart do
+  context 'Iteration 2' do
+    it 'exists' do
+      cart = ShoppingCart.new("King Soopers", "30items")
+
+      expect(cart).to be_an_instance_of(ShoppingCart)
+    end
+
+    it 'has a name' do
+      cart = ShoppingCart.new("King Soopers", "30items")
+
+      expect(cart.name).to eq("King Soopers")
+    end
+
+    it 'has a capacity' do
+      cart = ShoppingCart.new("King Soopers", "30items")
+
+      expect(cart.capacity).to eq(30)
+    end
+
+    it 'has an array for products' do
+      cart = ShoppingCart.new("King Soopers", "30items")
+
+      expect(cart.products).to eq([])
+    end
+
+    it 'can add products to itself' do
+      cart = ShoppingCart.new("King Soopers", "30items")
+      product1 = Product.new(:paper, 'toilet paper', 3.70, '10')
+      product2 = Product.new(:meat, 'chicken', 4.50, '2')
+      cart.add_product(product1)
+      cart.add_product(product2)
+
+      expect(cart.products).to eq([product1, product2])
+    end
+
+    it 'can return details about itself' do
+      cart = ShoppingCart.new("King Soopers", "30items")
+
+      expect(cart.details).to eq({name: "King Soopers", capacity: 30})
+    end
+
+  end
+end
