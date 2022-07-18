7359fb6bb383ffd5cb46f51d9db147139722db32
Riley McCullough <rileybmccullough@gmail.com>
2022-07-17 21:37:46 -0600

    Test: can return number of products in cart

--- spec/shopping_cart_spec.rb
+++ spec/shopping_cart_spec.rb
@@ -7,6 +7,8 @@ RSpec.describe ShoppingCart do
     @cart = ShoppingCart.new("King Soopers", "30items")
     @product1 = Product.new(:paper, 'toilet paper', 3.70, '10')
     @product2 = Product.new(:meat, 'chicken', 4.50, '2')
+    @product3 = Product.new(:paper, 'tissue paper', 1.25, '1')
+
   end
 
   it 'can name store currently shopping at' do
@@ -28,4 +30,23 @@ RSpec.describe ShoppingCart do
     expect(@cart.products.count).to eq(2)
     expect(@cart.details).to eq({name: "King Soopers", capacity: 30})
   end
+
+  it 'can return number of products in cart' do
+    @cart.add_product(@product1)
+    @cart.add_product(@product2)
+    @cart.add_product(@product3)
+
+    expect(@cart.total_number_of_products).to eq(13)
+  end
+    #
+    # expect(@cart.is_full?).to eq(false)
+    #
+    # product4 = Product.new(:produce, 'apples', 0.99, '20')
+    #
+    # @cart.add_product(@product4)
+    #
+    # expect(@cart.is_full?).to eq(true)
+    #
+    # expect(cart.products_by_category(:paper).count).to eq(2)
+
 end
