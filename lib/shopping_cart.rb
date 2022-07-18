68c3a91a1012ad635bdb8953dde583af1fc3a4d0
Aleisha Mork <aleisha.mork@gmail.com>
2022-07-18 10:42:22 -0600

    feat: Add method to check if cart is full and method to check products by category

--- lib/shopping_cart.rb
+++ lib/shopping_cart.rb
@@ -16,4 +16,23 @@ end
 def details
   details = {name: @name, capacity: @capacity}
 end
+
+def total_number_of_products
+  total = 0
+  @products.each do |product|
+    total += product.quantity
+  end
+  total
+end
+
+def is_full?
+  total_number_of_products > @capacity
+end
+
+def products_by_category(category)
+  products.find_all do |product|
+    product.category == category
+  end
+end
+
 end
--- spec/shopping_cart_spec.rb
+++ spec/shopping_cart_spec.rb
@@ -4,45 +4,89 @@ require './lib/shopping_cart'
 describe ShoppingCart do
   context 'Iteration 2' do
     it 'Shopping Cart exists' do
-      shopping_cart = ShoppingCart.new("King Soopers", "30items")
+      cart = ShoppingCart.new("King Soopers", "30items")
 
-      expect(shopping_cart).to be_an_instance_of ShoppingCart
+      expect(cart).to be_an_instance_of ShoppingCart
     end
 
     it 'Shopping Cart has a name' do
-      shopping_cart = ShoppingCart.new("King Soopers", "30items")
+      cart = ShoppingCart.new("King Soopers", "30items")
 
-      expect(shopping_cart.name).to eq "King Soopers"
+      expect(cart.name).to eq "King Soopers"
     end
 
     it 'Shopping Cart has a capacity' do
-      shopping_cart = ShoppingCart.new("King Soopers", "30items")
+      cart = ShoppingCart.new("King Soopers", "30items")
 
-      expect(shopping_cart.capacity).to eq 30
+      expect(cart.capacity).to eq 30
     end
 
     it 'Shopping Cart can hold products' do
-      shopping_cart = ShoppingCart.new("King Soopers", "30items")
+      cart = ShoppingCart.new("King Soopers", "30items")
 
-      expect(shopping_cart.products).to eq []
+      expect(cart.products).to eq []
     end
 
     it 'Shopping Cart can have products added' do
-      shopping_cart = ShoppingCart.new("King Soopers", "30items")
+      cart = ShoppingCart.new("King Soopers", "30items")
       product1 = Product.new(:paper, 'toilet paper', 3.70, '10')
       product2 = Product.new(:meat, 'chicken', 4.50, '2')
 
-      shopping_cart.add_product(product1)
-      shopping_cart.add_product(product2)
+      cart.add_product(product1)
+      cart.add_product(product2)
 
-      expect(shopping_cart.products).to eq [product1, product2]
+      expect(cart.products).to eq [product1, product2]
     end
 
     it "Shopping Cart can provide its details" do
-      shopping_cart = ShoppingCart.new("King Soopers", "30items")
+      cart = ShoppingCart.new("King Soopers", "30items")
 
-      expect(shopping_cart.details).to eq ({name: "King Soopers", capacity: 30})
+      expect(cart.details).to eq ({name: "King Soopers", capacity: 30})
     end
 
+    it "counts the number of products it contains" do
+      cart = ShoppingCart.new("King Soopers", "30items")
+      product1 = Product.new(:paper, 'toilet paper', 3.70, '10')
+      product2 = Product.new(:meat, 'chicken', 4.50, '2')
+      product3 = Product.new(:paper, 'tissue paper', 1.25, '1')
+
+      cart.add_product(product1)
+      cart.add_product(product2)
+      cart.add_product(product3)
+
+      expect(cart.total_number_of_products).to eq 13
+    end
+
+    it "Shopping Cart knows when it is full" do
+      cart = ShoppingCart.new("King Soopers", "30items")
+      product1 = Product.new(:paper, 'toilet paper', 3.70, '10')
+      product2 = Product.new(:meat, 'chicken', 4.50, '2')
+      product3 = Product.new(:paper, 'tissue paper', 1.25, '1')
+
+      cart.add_product(product1)
+      cart.add_product(product2)
+      cart.add_product(product3)
+
+      expect(cart.is_full?).to eq false
+
+      product4 = Product.new(:produce, 'apples', 0.99, '20')
+
+      cart.add_product(product4)
+
+      expect(cart.is_full?).to eq true
+    end
+
+    it "Shopping cart can list its products by category" do
+      cart = ShoppingCart.new("King Soopers", "30items")
+      product1 = Product.new(:paper, 'toilet paper', 3.70, '10')
+      product2 = Product.new(:meat, 'chicken', 4.50, '2')
+      product3 = Product.new(:paper, 'tissue paper', 1.25, '1')
+      product4 = Product.new(:produce, 'apples', 0.99, '20')
+      cart.add_product(product1)
+      cart.add_product(product2)
+      cart.add_product(product3)
+
+      expect(cart.products_by_category(:paper)).to eq [product1, product3]
+    end
   end
 end
