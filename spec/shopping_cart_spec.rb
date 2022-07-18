5cb08b135ca04e288922081fe5e637d5c2ec71cc
Riley McCullough <rileybmccullough@gmail.com>
2022-07-17 21:48:54 -0600

    Test: can confirm cart is full

--- spec/shopping_cart_spec.rb
+++ spec/shopping_cart_spec.rb
@@ -8,7 +8,7 @@ RSpec.describe ShoppingCart do
     @product1 = Product.new(:paper, 'toilet paper', 3.70, '10')
     @product2 = Product.new(:meat, 'chicken', 4.50, '2')
     @product3 = Product.new(:paper, 'tissue paper', 1.25, '1')
-
+    @product4 = Product.new(:produce, 'apples', 0.99, '20')
   end
 
   it 'can name store currently shopping at' do
@@ -47,13 +47,15 @@ RSpec.describe ShoppingCart do
     expect(@cart.is_full?).to eq(false)
   end
 
-  # it '' do
-  #   product4 = Product.new(:produce, 'apples', 0.99, '20')
-  #
-  #   @cart.add_product(@product4)
-  #
-  #   expect(@cart.is_full?).to eq(true)
-  #
-  #   expect(cart.products_by_category(:paper).count).to eq(2)
+  it 'can confirm cart is full' do
+    @cart.add_product(@product1)
+    @cart.add_product(@product2)
+    @cart.add_product(@product3)
+    @cart.add_product(@product4)
+
+    expect(@cart.is_full?).to eq(true)
+  end
+
+    # expect(@cart.products_by_category(:paper).count).to eq(2)
 
 end
