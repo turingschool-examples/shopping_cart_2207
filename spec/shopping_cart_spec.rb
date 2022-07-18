6ca0b15fcfa33c4a4a360ec3b0f78f5f6ba8fa75
Madeline Mauser <madeline.mauser@gmail.com>
2022-07-18 10:53:38 -0600

    Add tests for Iteration 3

--- spec/shopping_cart_spec.rb
+++ spec/shopping_cart_spec.rb
@@ -44,4 +44,43 @@ describe ShoppingCart do
       expect(cart.details).to eq({name: "King Soopers", capacity: 30})
     end
   end
+
+  context 'Iteration 3' do
+    before :each do
+      @cart = ShoppingCart.new("King Soopers", "30items")
+      @product1 = Product.new(:paper, 'toilet paper', 3.70, '10')
+      @product2 = Product.new(:meat, 'chicken', 4.50, '2')
+      @product3 = Product.new(:paper, 'tissue paper', 1.25, '1')
+      @product4 = Product.new(:produce, 'apples', 0.99, '20')
+    end
+
+    it 'can return the total number of products' do
+      @cart.add_product(@product1)
+      @cart.add_product(@product2)
+      @cart.add_product(@product3)
+
+      expect(@cart.total_number_of_products).to eq(13)
+    end
+
+    it 'can check whether it is full' do
+      @cart.add_product(@product1)
+      @cart.add_product(@product2)
+      @cart.add_product(@product3)
+
+      expect(@cart.is_full?).to eq(false)
+
+      @cart.add_product(@product4)
+
+      expect(@cart.is_full?).to eq(true)
+    end
+
+    it 'can return products_by_category' do
+      @cart.add_product(@product1)
+      @cart.add_product(@product2)
+      @cart.add_product(@product3)
+      @cart.add_product(@product4)
+
+      expect(@cart.products_by_category(:paper)).to eq([@product1, @product3])
+    end
+  end
 end
