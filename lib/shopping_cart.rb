ed32f2addd30424d3f8475725bab68de4fdabf66
Sergio Azcona <sergioazcona@Sergios-MBP.home>
2022-07-18 19:47:22 -0400

    feat/test: stuck on details method

--- lib/shopping_cart.rb
+++ lib/shopping_cart.rb
@@ -1,10 +1,19 @@
+require './lib/product'
 
 class ShoppingCart
-  attr_reader :name, :capacity
+  attr_reader :name, :capacity, :products
   def initialize(name, capacity)
     @name = name
-    @capacity = capacity
+    @capacity = capacity.to_i
+    @products = []
   end
 
+  def add_product(item)
+    products << item
+  end
+
+  def details
+    Product.name. Product.quantity 
+  end
 
 end
--- spec/shopping_cart_spec.rb
+++ spec/shopping_cart_spec.rb
@@ -3,10 +3,37 @@ require './lib/shopping_cart'
 
 RSpec.describe ShoppingCart do
   let(:cart) { ShoppingCart.new("King Soopers", "30items") }
-
+  let(:product1) { Product.new(:paper, 'toilet paper', 3.70, '10') }
+  let(:product2) { Product.new(:meat, 'chicken', 4.50, '2') }
+  
   it 'should be a class' do
     expect(cart).to be_an_instance_of ShoppingCart
   end
+
+  it 'should reflect capacity' do
+    expect(cart.capacity).to eq 30
+  end
+
+  it 'should return the product array' do
+    expect(cart.products).to eq ([])
+  end
+
+  xit 'should be able to add products to cart' do
+    cart.add_product(product1)
+    cart.add_product(product2) 
+# require 'pry';binding.pry
+    expect(cart.products).to eq(product1)
+        #output is not within array brackets
+  end
+
+  it 'should inform what items are in the cart' do
+    cart
+    expect(cart.details).to eq(cart)
+    
+  end
+
+
+
 
 
 end
