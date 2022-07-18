cf859e8d5ff230bb9190266f0c9b90975b8dec67
Benjamin Randolphgit config --global user.email neb417@gmail.comgit config --global init.defaultBranch maingit config --global core.editor atom <neb417@gmail.com>
2022-07-17 19:07:03 -0600

    Add #total_sum_od_products, #is_cart_full?, #products_by_category and test. Complete iteration 3

--- lib/shopping_cart.rb
+++ lib/shopping_cart.rb
@@ -9,11 +9,28 @@ class ShoppingCart
     @name = name
     @capacity = capacity.to_i
     @products = []
-    @details = {name: name, capacity: @capacity}
+    @details = {name: @name, capacity: @capacity}
+    @cart_capacity = 0
   end  
 
   def add_product(product)
     @products << product
   end
 
+  def total_number_of_products
+    @products.sum do |product|
+      total += product.quantity
+    end
+  end
+
+  def is_cart_full?
+    total_number_of_products > @capacity
+  end
+
+  def products_by_capacity
+
+    @products.find_all? do |product|
+      product.category == category
+    end
+  end
 end
--- spec/shopping_cart_spec.rb
+++ spec/shopping_cart_spec.rb
@@ -42,4 +42,46 @@ describe ShoppingCart
 
       expect(@cart.details).to eq(name: "King Soopers", capacity: 30)
     end
+
+    it 'counts products in the cart' do
+      product1 = Product.new(:paper, 'toilet paper', 3.70, '10')
+      product2 = Product.new(:meat, 'chicken', 4.50, '2')
+      product3 = Product.new(:paper, 'tissue paper', 1.25, '1')
+
+      @cart.add_product(product1)
+      @cart.add_product(product2)
+      @cart.add_product(product3)
+      expect(@cart.total_number_of_products).to eq 13
+    end
+
+    it 'has can fill up' do
+      product1 = Product.new(:paper, 'toilet paper', 3.70, '10')
+      product2 = Product.new(:meat, 'chicken', 4.50, '2')
+      product3 = Product.new(:paper, 'tissue paper', 1.25, '1')
+
+      @cart.add_product(product1)
+      @cart.add_product(product2)
+      @cart.add_product(product3)
+
+      expect(@cart.is_cart_full?).to eq false
+
+      product4 = Product.new(:produce, 'apples', 0.99, '20')
+      @cart.add_product(product4)
+
+      expect(@cart.is_cart_full?).to eq true
+    end
+
+    it 'finds products by category' do
+      product1 = Product.new(:paper, 'toilet paper', 3.70, '10')
+      product2 = Product.new(:meat, 'chicken', 4.50, '2')
+      product3 = Product.new(:paper, 'tissue paper', 1.25, '1')
+      product4 = Product.new(:produce, 'apples', 0.99, '20')
+
+      @cart.add_product(product1)
+      @cart.add_product(product2)
+      @cart.add_product(product3)
+      @cart.add_product(product4)
+
+      expect(cart.products_by_category(:paper)).to eq product1, product3
+    end
   end
